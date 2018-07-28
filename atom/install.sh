4d902d633229f4c553ad643ed876ea0d93643ad0
ben <isieldor@gmail.com>
2018-07-28 14:47:58 -0500

    install.sh is now correct with respect to arch

--- atom/install.sh
+++ atom/install.sh
@@ -1,6 +1,4 @@
-
 apm install atom-material-ui atom-material-syntax vim-mode-plus
 npm install -g node-gyp
 apm install platformio-ide-terminal advanced-open-file prettier-atom ex-mode
-#need to figure out where .atom is before the below line will work
-#cat styles.less >> %userprofile%\.atom\styles.less
+cat styles.less >> ~/.atom/styles.less
