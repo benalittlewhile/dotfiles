c6165c6ad3d33bfd6c59418246877296b8fa7d56
ben <isieldor@gmail.com>
2018-07-28 14:37:37 -0500

    added vim install script

---
+++ vim/vimstall.sh
@@ -0,0 +1,26 @@
+#setup my vimrc
+echo "source ~/dev/main/dotbackup/vim/vimrc" >> ~/.vimrc
+
+#install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#install hybrid-material theme
+git clone https://github.com/kristijanhusak/vim-hybrid-material ~/.vim/bundle/vim-hybrid-material
+
+#install airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+#install vim-surround
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
+
+#install nerd tree file browser
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree
+
+#install emmet
+cd ~/.vim/bundle
+git clone https://github.com/mattn/emmet-vim.git
