e8706dc2f02cacd1fb1944c8bf7009c07fc83e00
ben <isieldor@gmail.com>
2018-08-03 13:28:15 -0500

    added fix for atom ctrl-tab behaviour to scripts

--- atom/install.bat
+++ atom/install.bat
@@ -2,5 +2,6 @@ call apm install atom-material-ui atom-material-syntax vim-mode-plus
 call npm install -g node-gyp
 call apm install platformio-ide-terminal advanced-open-file prettier-atom ex-mode
 cat styles.less >> %userprofile%\.atom\styles.less
+cat keymap.cson >> %userprofile%\.atom\keymap.cson
 rm -rf node_modules
 rm -rf package-lock.json
--- atom/install.sh
+++ atom/install.sh
@@ -2,3 +2,4 @@ apm install atom-material-ui atom-material-syntax vim-mode-plus
 npm install -g node-gyp
 apm install platformio-ide-terminal advanced-open-file prettier-atom ex-mode
 cat styles.less >> ~/.atom/styles.less
+cat keymap.cson >> ~/.atom/keymap.cson
