a67b702cc9397cebc05a2c369036c42b0beb53e8
ben <isieldor@gmail.com>
2020-02-19 14:52:22 -0600

    added new keybindings and backup script

---
+++ backup.sh
@@ -0,0 +1,13 @@
+# script to copy latest versions of settings files to this
+# repo
+
+#vs code
+#clear old versions
+rm vs\ code/settings.json
+rm vs\ code/keybindings.json
+rm vs\ code/extensions.txt
+
+#copy current versions to repo
+code --list-extensions >> vs\ code/extensions.txt
+cp ~/Library/Application\ Support/Code/User/settings.json vs\ code/settings.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json vs\ code/keybindings.json
--- vs code/install.sh
+++ vs code/install.sh
@@ -1,2 +1,3 @@
 cat vs_code_extensions_list.txt | xargs -n 1 code --install-extension
 cp settings.json ~/Library/Application\ Support/Code/User/settings.json
+cp keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
