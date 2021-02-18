0aaf03b3af65f280a48044360009df79973a8110
ben <isieldor@gmail.com>
2021-02-18 15:03:59 -0600

    added script to install my prompt... probably

---
+++ iterm2/installPrettyPrompt.sh
@@ -0,0 +1,7 @@
+git clone https://gitlab.com/jallbrit/bonsai.sh ~/bin/bonsai.sh
+chmod +x ~/bin/bonsai.sh/bonsai.sh
+ln -s ~/bin/bonsai.sh/bonsai.sh ~/.local/bin/bonsai
+
+echo "bonsai.sh -n -L 30 -g 35,20 > ~/.my_bonsai_art.txt
+neofetch --ascii ~/.my_bonsai_art.txt --ascii_colors 11 3 10 2 0
+cd ~/dev" > ~/.zshrc
