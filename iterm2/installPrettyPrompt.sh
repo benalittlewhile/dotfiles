06994e967cd6bce52ab920a59c17e2f91fa6dd20
Ben <isieldor@gmail.com>
2021-02-18 15:07:14 -0600

    needed to create folder before linking

--- iterm2/installPrettyPrompt.sh
+++ iterm2/installPrettyPrompt.sh
@@ -1,5 +1,6 @@
 git clone https://gitlab.com/jallbrit/bonsai.sh ~/bin/bonsai.sh
 chmod +x ~/bin/bonsai.sh/bonsai.sh
+mkdir -p ~/.local/bin
 ln -s ~/bin/bonsai.sh/bonsai.sh ~/.local/bin/bonsai
 
 echo "bonsai.sh -n -L 30 -g 35,20 > ~/.my_bonsai_art.txt
